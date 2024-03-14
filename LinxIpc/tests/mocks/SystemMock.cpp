962b7251179f34ee7d1911da7165999e1ba25c04
Bartlomiej Bury <bartekbury@gmail.com>
2024-03-14 14:47:33 +0000

    auto generate mocks proxy

--- LinxIpc/tests/mocks/SystemMock.cpp
+++
@@ -1,8 +0,0 @@
-#include "SystemMock.h"
-
-CMOCK_MOCK_FUNCTION(SystemMock, int, clock_gettime, (clockid_t clk_id, struct timespec *tp));
-CMOCK_MOCK_FUNCTION(SystemMock, int, usleep, (uint64_t usec));
-CMOCK_MOCK_FUNCTION(SystemMock, ssize_t, read, (int fd, void *buf, size_t nbytes));
-CMOCK_MOCK_FUNCTION(SystemMock, ssize_t, write, (int fd, const void *buf, size_t n));
-CMOCK_MOCK_FUNCTION(SystemMock, int, close, (int fd));
-CMOCK_MOCK_FUNCTION(SystemMock, int, eventfd, (unsigned int count, int flags));
--- cmake/top.cmake
+++ cmake/top.cmake
@@ -2,6 +2,59 @@ option(UNIT_TESTS "build unit tests" FALSE)
 option(COVERITY "enable coverity" FALSE)
 option(VALGRIND "enable valgrind" FALSE)
 
+define_property(GLOBAL PROPERTY UNIT_TEST_MOCKS BRIEF_DOCS "aa " FULL_DOCS "mock headers")
+define_property(GLOBAL PROPERTY UNIT_TEST_SOURCES BRIEF_DOCS "aa " FULL_DOCS "ut sources")
+define_property(GLOBAL PROPERTY UNIT_TEST_MOCKS_DIR BRIEF_DOCS "aa " FULL_DOCS "ut sources")
+
+define_property(GLOBAL PROPERTY UNIT_TEST_TARGET_DEFINES BRIEF_DOCS "aa " FULL_DOCS "target defines")
+define_property(GLOBAL PROPERTY UNIT_TEST_TARGET_SOURCES BRIEF_DOCS "aa " FULL_DOCS "target sources")
+define_property(GLOBAL PROPERTY UNIT_TEST_TARGET_INCLUDES BRIEF_DOCS "aa " FULL_DOCS "target includes")
+
+function(add_to_ut)
+
+    set(options OPTIONAL "")
+    set(oneValueArgs TARGET)
+    set(multiValueArgs SOURCES MOCKS)
+    cmake_parse_arguments(ADD_TO_UT "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN} )
+
+    get_property(ut_sources GLOBAL PROPERTY UNIT_TEST_SOURCES)
+    list(APPEND ut_sources ${ADD_TO_UT_SOURCES})
+    set_property(GLOBAL PROPERTY UNIT_TEST_SOURCES ${ut_sources})
+
+    get_property(ut_mocks_dir GLOBAL PROPERTY UNIT_TEST_MOCKS_DIR)
+    list(APPEND ut_mocks_dir ${ADD_TO_UT_MOCKS})
+    set_property(GLOBAL PROPERTY UNIT_TEST_MOCKS_DIR ${ut_mocks_dir})
+
+    foreach(file ${ADD_TO_UT_MOCKS})
+        file(GLOB mocks "${file}/*Mock.h")
+
+        get_property(current_mocks GLOBAL PROPERTY UNIT_TEST_MOCKS)
+        list(APPEND current_mocks ${mocks})
+        set_property(GLOBAL PROPERTY UNIT_TEST_MOCKS ${current_mocks})
+
+    endforeach()
+                
+    get_target_property(TARGET_SOURCES ${ADD_TO_UT_TARGET} SOURCES)
+    get_target_property(TARGET_INCLUDES ${ADD_TO_UT_TARGET} INCLUDE_DIRECTORIES)
+    get_target_property(TARGET_DEFINITIONS ${ADD_TO_UT_TARGET} COMPILE_DEFINITIONS)
+
+    if(TARGET_DEFINITIONS)
+        get_property(current_defines GLOBAL PROPERTY UNIT_TEST_TARGET_DEFINES)
+        list(APPEND current_defines ${TARGET_DEFINITIONS})
+        set_property(GLOBAL PROPERTY UNIT_TEST_TARGET_DEFINES ${current_defines})
+    endif()
+
+    get_property(current_sources GLOBAL PROPERTY UNIT_TEST_TARGET_SOURCES)
+    list(APPEND current_sources ${TARGET_SOURCES})
+    set_property(GLOBAL PROPERTY UNIT_TEST_TARGET_SOURCES ${current_sources})
+
+    get_property(current_includes GLOBAL PROPERTY UNIT_TEST_TARGET_INCLUDES)
+    list(APPEND current_includes ${TARGET_INCLUDES})
+    set_property(GLOBAL PROPERTY UNIT_TEST_TARGET_INCLUDES ${current_includes})
+
+endfunction()
+
 if(NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE Release)
 endif()
@@ -17,13 +70,6 @@ add_link_options(
     -Wl,-Map=output.map
     -Wl,--gc-sections
 )
-
-if(UNIT_TESTS)
-    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/ut.cmake)
-else()
-    function(add_to_ut)
-    endfunction()
-endif()
 
 message(STATUS "#################################")
 message(STATUS "     BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
